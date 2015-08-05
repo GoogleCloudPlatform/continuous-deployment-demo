7f80736adcbdbec330c34fa89eccd1bbc3584c85
Bill Prin <waprin@google.com>
2015-08-04 23:20:53 -0700

    small test

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from api_key import key
 
 @app.route('/get_author/<title>')
 def get_author(title):
-    host = 'https://www.googleapis.com/books/v1/volumes?q={}&key={}&country=US'.format(title, key)
+    host = 'https://www.googleapis.com/books/v1/volume?q={}&key={}&country=US'.format(title, key)
     request = urllib2.Request(host)
     try:
         response = urllib2.urlopen(request)
