5d3b35937073e692679709e8cbc4e0cab99f2abe
ashware <amir@ashrafzadeh.net>
2019-08-30 13:02:41 +1000

    Update test_main.py
    to fail

--- test_main.py
+++ test_main.py
@@ -23,7 +23,7 @@ class MainTest(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/get_author/ulysses')
-        assert rv.data == 'James Joyce'
+        assert rv.data == 'James Joycee'
 
 if __name__ == '__main__':
     unittest.main()
