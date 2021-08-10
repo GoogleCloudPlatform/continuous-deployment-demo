0bdc95bbe692db0f5f04fbda20cb05e76670cd0a
shenxiang <shenxiang@google.com>
2021-08-10 04:33:27 +0000

    update to python3 etc.

--- test_main.py
+++ test_main.py
@@ -23,7 +23,8 @@ class MainTest(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/get_author/ulysses')
-        assert rv.data == 'James Joyce'
+        assert rv.status_code == 200
+        assert rv.data.decode('utf-8') == 'James Joyce'
 
 if __name__ == '__main__':
     unittest.main()
